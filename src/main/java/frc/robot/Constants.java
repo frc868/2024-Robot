87fd5e52db82c10bc09d505f49f41d3409b2fbb9
Kev1n8088 <jebidiahkerman828@gmail.com>
2024-02-01 16:44:21 -0500

    added constants file

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+public class Constants {
+    enum ControllerType {
+        XboxController,
+        FlightStick
+    }    
+    
+    public static final boolean DEBUG_MODE = false;
+
+    public static final ControllerType CONTROLLER_TYPE = ControllerType.FlightStick;
+
+    public static final class Drivetrain {
+        
+    }
+
+
+
+    
+}
