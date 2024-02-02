3b3b5fcc3075e4d74fc0f9c903447f0760e34953
Amruta Killampalli <ksavarshini@gmail.com>
2024-02-02 18:02:51 -0500

    Create Constants.java
    Created Constants.java file with a Constants and Intake class

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkFlex;
+
+public class Constants {
+    public static final class Intake {
+        public static final int CURRENT_LIMIT = 0;
+        public static final double ENCODER_ROTATIONS_TO_RADIANS = 0.0;
+        public static final double RESER_POSITION = 90.0;
+        public static final int INTAKE_MOTOR_ID = 0;
+        public static final int LIFTING_MOTOR_1_ID = 1;
+        public static final int LIFTING_MOTOR_2_ID = 2;
+        public static final int BEAM_BREAK_ID = 0; 
+
+    }    
+}
