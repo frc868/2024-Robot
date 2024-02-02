752c38326c218377f5db585200efbe0088b72ae8
Kev1n8088 <jebidiahkerman828@gmail.com>
2024-02-02 16:40:29 -0500

    filled getAprilTagPoses

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,13 +1,20 @@
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 
 import com.techhounds.houndutil.houndlib.AprilTagPhotonCamera;
 import com.techhounds.houndutil.houndlib.subsystems.BaseVision;
+import com.techhounds.houndutil.houndlog.interfaces.Log;
 
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase implements BaseVision {
@@ -23,10 +30,18 @@ public class Vision extends SubsystemBase implements BaseVision {
     public void updatePoseEstimator() {
     }
 
+    @Log
     public Pose3d[] getCameraPoses() {
     }
 
+    @Log
     public Pose3d[] getAprilTagPoses() {
+        List<Pose3d> poses = new ArrayList<Pose3d>();
+        for (AprilTag tag : AprilTagFields.kDefaultField.loadAprilTagLayoutField().getTags()) {
+            poses.add(tag.pose);
+        }
+        Pose3d[] poseArray = new Pose3d[poses.size()];
+        return poses.toArray(poseArray);
     }
 
     public void setPoseEstimator(SwerveDrivePoseEstimator poseEstimator) {
