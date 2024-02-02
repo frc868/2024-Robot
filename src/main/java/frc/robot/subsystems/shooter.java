5aa11b9a40f23f5f8d05002dd61ef7c3900fb947
ZorzalG <nickwillimJT2@gmail.com>
2024-02-02 16:20:01 -0500

    create all basic functions through mostly copy paste

--- src/main/java/frc/robot/subsystems/shooter.java
+++ src/main/java/frc/robot/subsystems/shooter.java
@@ -2,45 +2,109 @@ package frc.robot.subsystems;
 
 import java.util.function.Supplier;
 
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkFlex;
+import com.techhounds.houndutil.houndlib.SparkConfigurator;
 import com.techhounds.houndutil.houndlib.subsystems.BaseShooter;
+import com.techhounds.houndutil.houndlog.interfaces.Log;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
 import com.revrobotics.CANSparkLowLevel;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 
-public class shooter extends SubsystemBase implements BaseShooter {
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+
+import static frc.robot.Constants.Shooter.*;
+
+public class Shooter extends SubsystemBase implements BaseShooter {
 
     //example motor
-    public CANSparkFlex yay = new CANSparkFlex(0, CANSparkLowLevel.MotorType.kBrushless);
+    private CANSparkFlex Left;
+    private CANSparkFlex Right;
     //example velocity variable
-    public double velocity;
+
+    private SimpleMotorFeedforward feedforwardController = new SimpleMotorFeedforward(kS, kV, kA);
+    private PIDController PIDController = new PIDController(kP,kI,kD);
+
+    @Log(groups = "control")
+    private double feedbackVoltage = 0;
+    @Log(groups = "control")
+    private double feedforwardVoltage = 0;
+
+
+    public Shooter(){
+        Left = SparkConfigurator.createSparkFlex(
+                LEFT_MOTOR_ID, MotorType.kBrushless, false,
+                (s) -> s.setIdleMode(IdleMode.kBrake),
+                (s) -> s.setSmartCurrentLimit(CURRENT_LIMIT),
+                (s) -> s.getEncoder().setPositionConversionFactor(ENCODER_ROTATIONS_TO_METERS),
+                (s) -> s.getEncoder().setVelocityConversionFactor(ENCODER_ROTATIONS_TO_METERS / 60.0));
+        Right = SparkConfigurator.createSparkFlex(
+                RIGHT_MOTOR_ID, MotorType.kBrushless, false,
+                (s) -> s.setIdleMode(IdleMode.kBrake),
+                (s) -> s.setSmartCurrentLimit(CURRENT_LIMIT),
+                (s) -> s.getEncoder().setPositionConversionFactor(ENCODER_ROTATIONS_TO_METERS),
+                (s) -> s.getEncoder().setVelocityConversionFactor(ENCODER_ROTATIONS_TO_METERS / 60.0));
+        
+
+    }
+
+    public double getPosition(){
+        return (Left.getEncoder().getPosition() + Right.getEncoder().getPosition()) / 2;
+    }
 
     @Override
     public double getVelocity() {
-        yay.getEncoder();
-        velocity = 1.23; //somehow find the speed (probably something with the encoder) ¯\_(ツ)_/¯
-        return(velocity);
+        return (Left.getEncoder().getVelocity() + Right.getEncoder().getVelocity()) / 2;
     }
 
     @Override
     public void setVoltage(double voltage) {
-        yay.setVoltage(voltage);
+        Left.setVoltage(voltage);
+        Right.setVoltage(voltage);
     }
 
     @Override
-    public Command spinAtVelocityCommand(Supplier<Double> goalVelocitySupplier) {
-        //I still think it's going to be more complex than this
-        return runOnce(() -> yay.setVoltage(12)); //gets voltage from PD or something
+    public Command spinAtVelocityCommand(Supplier<Double> goalVelocitySupplier) { //idk lol
+        return run(() -> {
+            feedbackVoltage = PIDController.calculate(getVelocity());
+            feedforwardVoltage = feedforwardController.calculate(goalVelocitySupplier.get());
+            setVoltage(feedbackVoltage + feedforwardVoltage);
+        });
     }
 
     @Override
-    public Command setOverridenSpeedCommand(Supplier<Double> speed) {
+    public Command spinAtCurrentVelocityCommand() { //idk lol
+        return runOnce(() -> spinAtVelocityCommand(() -> getVelocity()));
 
     }
 
     @Override
-    public Command coastMotorsCommand() {
-
+    public Command setOverridenSpeedCommand(Supplier<Double> speed) { //idk
+        return run(() -> setVoltage(12.0 * speed.get()))
+                .withName("Set Overridden Shooter Speed");
     }
+
+    @Override
+    public Command coastMotorsCommand() { //idk
+        return runOnce(() -> Left.stopMotor())
+                .andThen(() -> {
+                    Left.setIdleMode(IdleMode.kCoast);
+                    Right.setIdleMode(IdleMode.kCoast);
+                })
+                .finallyDo((d) -> {
+                    Left.setIdleMode(IdleMode.kBrake);
+                    Right.setIdleMode(IdleMode.kBrake);
+                    PIDController.reset();
+                }).withInterruptBehavior(InterruptionBehavior.kCancelIncoming);
+    }
+
 }
