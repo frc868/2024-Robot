ac5c1fbe2dab5742ff556ae9de49d80064538b2e
Amruta Killampalli <ksavarshini@gmail.com>
2024-02-02 18:04:43 -0500

    Implement methods
    Implemented stopIntake, startIntake, setIntakePosition, and resetPose.

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,34 +1,58 @@
 package frc.robot.subsystems;
+
 import java.util.function.Function;
 
 import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.REVLibError;
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.techhounds.houndutil.houndlib.SparkConfigurator;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Intake {
+import static frc.robot.Constants.Intake.*;
+
+/*
+ * The Intake subsystem, which pulls in a note and can be sent to a shooter or can be used to score the amp. 
+ * @author rb, jq, ak
+ */
+public class Intake extends SubsystemBase {
     private DigitalInput beamBreak;
-    //Function<CANSparkBase, REVLibError> frontMotor2 = frontMotor2::setSmartCurrentLimit;
+
+    // Function<CANSparkBase, REVLibError> frontMotor2 =
+    // frontMotor2::setSmartCurrentLimit;
     public Intake() {
-        SparkConfigurator.createSparkFlex(0, MotorType.kBrushless, false, (s) -> s.setIdleMode(IdleMode.kBrake, (s) -> s.setSmartCurrentLimit(CURRENT_LIMIT), (s) -> s.getEncoder().setPositionConversionFactor(ENCODER_ROTATIONS_TO_RADIANS),(s) -> s.getEncoder().setVelocityConversionFactor(ENCODER_ROTATIONS_TO_RADIANS / 60.0)));
-        SparkConfigurator.createSparkFlex(1, MotorType.kBrushless, false, Function<>);
-        SparkConfigurator.createSparkFlex(2, MotorType.kBrushless, false, Function<>);
-        beamBreak = new DigitalInput(0);
+        SparkConfigurator.createSparkFlex(INTAKE_MOTOR_ID, MotorType.kBrushless, false,
+                (s) -> s.setIdleMode(IdleMode.kBrake), (s) -> s.setSmartCurrentLimit(CURRENT_LIMIT),
+                (s) -> s.getEncoder().setPositionConversionFactor(ENCODER_ROTATIONS_TO_RADIANS),
+                (s) -> s.getEncoder().setVelocityConversionFactor(ENCODER_ROTATIONS_TO_RADIANS / 60.0));
+        SparkConfigurator.createSparkFlex(LIFTING_MOTOR_1_ID, MotorType.kBrushless, false,
+                (s) -> s.setIdleMode(IdleMode.kBrake), (s) -> s.setSmartCurrentLimit(CURRENT_LIMIT),
+                (s) -> s.getEncoder().setPositionConversionFactor(ENCODER_ROTATIONS_TO_RADIANS),
+                (s) -> s.getEncoder().setVelocityConversionFactor(ENCODER_ROTATIONS_TO_RADIANS / 60.0));
+        SparkConfigurator.createSparkFlex(LIFTING_MOTOR_2_ID, MotorType.kBrushless, false,
+                (s) -> s.setIdleMode(IdleMode.kBrake), (s) -> s.setSmartCurrentLimit(CURRENT_LIMIT),
+                (s) -> s.getEncoder().setPositionConversionFactor(ENCODER_ROTATIONS_TO_RADIANS),
+                (s) -> s.getEncoder().setVelocityConversionFactor(ENCODER_ROTATIONS_TO_RADIANS / 60.0));
+        beamBreak = new DigitalInput(BEAM_BREAK_CHANNEL);
     }
-    private CANSparkFlex frontMotor = new CANSparkFlex(0, MotorType.kBrushless);
-    private CANSparkFlex leftMotor = new CANSparkFlex(1, MotorType.kBrushless);
-    private CANSparkFlex rightMotor = new CANSparkFlex(2, MotorType.kBrushless);
+
     private double velocity;
 
-    public Command startIntake(){
-        
+    public Command startIntake(CANSparkFlex motor) {
+        return runOnce(() -> {
+            motor.setVoltage(1);
+        });
+
     }
 
-    public Command stopIntake(){
+    public Command stopIntake(CANSparkFlex motor) {
+        return runOnce(() -> {
+            motor.setVoltage(0);
+        });
 
     }
 
@@ -44,17 +68,18 @@ public class Intake {
 
     }
 
-    public double getIntakePosition(){
-
+    public double getIntakePosition(CANSparkFlex motor) {
+        return motor.getEncoder().getPosition();
     }
 
     public double getVelocity(CANSparkFlex motor) {
         velocity = motor.getEncoder().getVelocity();
-        return(velocity);
+        return (velocity);
     }
 
-    public void resetPose() {
-        
+    public void resetPose(CANSparkFlex motor1, CANSparkFlex motor2) {
+        motor1.getEncoder().setPosition(RESET_POSITION);
+        motor2.getEncoder().setPosition(RESET_POSITION);
     }
 
     public void setVoltage(double voltage, CANSparkFlex motor) {
@@ -62,4 +87,3 @@ public class Intake {
     }
 
 }
-
