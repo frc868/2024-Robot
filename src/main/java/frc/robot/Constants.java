bd73520b69e4d2e1f3efedd9dcc7b37430c74be1
sageryker <149901186+sageryker@users.noreply.github.com>
2024-02-03 12:08:27 -0500

    Add PID && feedforward constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,15 @@
 package frc.robot;
 
 public class Constants {
-    public static final class Climber{
+    public static final class Climber {
         public static final int CLIMBER_MOTOR_ID = 0;
+
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
+        public static final double kS = 0;
+        public static final double kG = 0;
+        public static final double kV = 0;
+        public static final double kA = 0;
     }
 }
