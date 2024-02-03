2c14f3be8712d75b8fd26cc62b24f02bfde4bd86
sageryker <149901186+sageryker@users.noreply.github.com>
2024-02-03 18:50:05 -0500

    Add ProfiledPID constraints(Place fillers)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+
 public class Constants {
     public static final class Climber {
         public static final int CLIMBER_MOTOR_ID = 0;
@@ -11,5 +13,11 @@ public class Constants {
         public static final double kG = 0;
         public static final double kV = 0;
         public static final double kA = 0;
+
+        public static final double MAX_VELOCITY_METERS_PER_SECOND = 0;
+        public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 0;
+
+        public static final TrapezoidProfile.Constraints MOVEMENT_CONSTRAINTS = new TrapezoidProfile.Constraints(
+                MAX_VELOCITY_METERS_PER_SECOND, MAX_ACCELERATION_METERS_PER_SECOND_SQUARED);
     }
 }
