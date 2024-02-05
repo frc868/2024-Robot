33cb7b229fb01478c0577a85505d5afb019f7f79
ZorzalG <nickwillimJT2@gmail.com>
2024-02-05 17:17:54 -0500

    Revert "Fix errors"
    This reverts commit 2ce497cf07eefa89ec4f86d4fb4cf42facb43645.

--- src/main/java/frc/robot/constants.java
+++ src/main/java/frc/robot/constants.java
@@ -1,20 +1,18 @@
 package frc.robot;
 
-public class constants {
+public class Constants {
     public static final class Shooter{
         public static final int LEFT_MOTOR_ID = 1337;
         public static final int RIGHT_MOTOR_ID = 1337;
-
         public static final int CURRENT_LIMIT = 1337;
-        
         public static final double ENCODER_ROTATIONS_TO_METERS = 1337;
-
         public static final double kS = 1337;
         public static final double kV = 1337;
         public static final double kA = 1337;
-
         public static final double kP = 1337;
         public static final double kI = 1337;
         public static final double kD = 1337;
+
+
     }
 }
--- src/main/java/frc/robot/subsystems/shooter.java
+++ src/main/java/frc/robot/subsystems/shooter.java
@@ -1,10 +1,9 @@
-package frc.robot.subsystems;
+package frc.robot.Subsystems;
 
 import java.util.function.Supplier;
 
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkFlex;
-
 import com.techhounds.houndutil.houndlib.SparkConfigurator;
 import com.techhounds.houndutil.houndlib.subsystems.BaseShooter;
 import com.techhounds.houndutil.houndlog.interfaces.Log;
@@ -12,15 +11,20 @@ import com.techhounds.houndutil.houndlog.interfaces.Log;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
+import com.revrobotics.CANSparkLowLevel;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.PIDController;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import static frc.robot.constants.Shooter.*;
 
-public class shooter extends SubsystemBase implements BaseShooter {
+import static frc.robot.Constants.Shooter.*;
+
+public class Shooter extends SubsystemBase implements BaseShooter {
 
     //example motor
     private CANSparkFlex Left;
@@ -36,20 +40,21 @@ public class shooter extends SubsystemBase implements BaseShooter {
     private double feedforwardVoltage = 0;
 
 
-    public void Shooter(){
+    public Shooter(){
         Left = SparkConfigurator.createSparkFlex(
                 LEFT_MOTOR_ID, MotorType.kBrushless, false,
                 (s) -> s.setIdleMode(IdleMode.kBrake),
                 (s) -> s.setSmartCurrentLimit(CURRENT_LIMIT),
                 (s) -> s.getEncoder().setPositionConversionFactor(ENCODER_ROTATIONS_TO_METERS),
                 (s) -> s.getEncoder().setVelocityConversionFactor(ENCODER_ROTATIONS_TO_METERS / 60.0));
-
         Right = SparkConfigurator.createSparkFlex(
                 RIGHT_MOTOR_ID, MotorType.kBrushless, false,
                 (s) -> s.setIdleMode(IdleMode.kBrake),
                 (s) -> s.setSmartCurrentLimit(CURRENT_LIMIT),
                 (s) -> s.getEncoder().setPositionConversionFactor(ENCODER_ROTATIONS_TO_METERS),
                 (s) -> s.getEncoder().setVelocityConversionFactor(ENCODER_ROTATIONS_TO_METERS / 60.0));
+        
+
     }
 
     public double getPosition(){
