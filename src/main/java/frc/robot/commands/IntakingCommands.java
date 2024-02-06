2b7031fc882336bcd2ea5707dc86b2f71ac89f5a
Amruta Killampalli <ksavarshini@gmail.com>
2024-02-05 20:34:41 -0500

    Begin implementing startAndRunIntakingCommand

--- src/main/java/frc/robot/commands/IntakingCommands.java
+++ src/main/java/frc/robot/commands/IntakingCommands.java
@@ -1,5 +1,11 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
 public class IntakingCommands {
+    public static Command startAndRunIntakingCommand() {
+        return Commands.sequence();
+    }
 
 }
