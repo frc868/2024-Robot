6ac86b115b628f51baceae2f11426aa62d2ae070
Amruta Killampalli <ksavarshini@gmail.com>
2024-02-07 17:49:05 -0500

    Make kS, kG, kV, kA constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final class Intake {
         public static final int CURRENT_LIMIT = 0;
         public static final double ENCODER_ROTATIONS_TO_RADIANS = 0.0;
-        public static final double RESER_POSITION = 90.0;
+        public static final double RESET_POSITION = 90.0;
         public static final int INTAKE_MOTOR_ID = 0;
         public static final int LIFTING_MOTOR_1_ID = 1;
         public static final int LIFTING_MOTOR_2_ID = 2;
@@ -17,5 +17,9 @@ public class Constants {
         public static final double kD = 0.0;
         public static final double maxVelocity = 0.0;
         public static final double maxAcceleration = 0.0;
+        public static final double kS = 0.0;
+        public static final double kG = 0.0;
+        public static final double kV = 0.0;
+        public static final double kA = 0.0;
     }
 }
