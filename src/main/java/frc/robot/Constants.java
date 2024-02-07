80c81a5699972b25bd5d5b8bec3c4b1953e20e11
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2024-02-07 17:54:10 -0500

    add untested comment

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,8 +5,8 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 public class Constants {
     public static final class Climber {
         public static enum ClimberPosition {
-            TOP(0),
-            BOTTOM(0);
+            TOP(0), // untested
+            BOTTOM(0); // untested
 
             public final double position;
 
@@ -17,16 +17,16 @@ public class Constants {
 
         public static final int CLIMBER_MOTOR_ID = 0;
 
-        public static final double kP = 0;
-        public static final double kI = 0;
-        public static final double kD = 0;
-        public static final double kS = 0;
-        public static final double kG = 0;
-        public static final double kV = 0;
-        public static final double kA = 0;
+        public static final double kP = 0; // untested
+        public static final double kI = 0; // untested
+        public static final double kD = 0; // untested
+        public static final double kS = 0; // untested
+        public static final double kG = 0; // untested
+        public static final double kV = 0; // untested
+        public static final double kA = 0; // untested
 
-        public static final double MAX_VELOCITY_METERS_PER_SECOND = 0;
-        public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 0;
+        public static final double MAX_VELOCITY_METERS_PER_SECOND = 0; // untested
+        public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 0; // untested
 
         public static final TrapezoidProfile.Constraints MOVEMENT_CONSTRAINTS = new TrapezoidProfile.Constraints(
                 MAX_VELOCITY_METERS_PER_SECOND, MAX_ACCELERATION_METERS_PER_SECOND_SQUARED);
