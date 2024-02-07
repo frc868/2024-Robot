2172e1f3634d51d893a297269d2918f9d349ef27
QuinnKenison <quinnkenison@gmail.com>
2024-02-06 19:02:26 -0500

    Address the comments on pull request
    - Organize imports
    - Make "Left" "primaryMotor" and "Right" "secondaryMotor"
    - Rename "PIDController" to "pidController"
    - Add comments on unnecessary methods
    - Make getVelocity() use only primaryMotor
    - Make spinAtVelocity() use goalVelocitySupplier input
    
    This will likely need to be finalized by Justin.

--- src/main/java/frc/robot/subsystems/shooter.java
+++ src/main/java/frc/robot/subsystems/shooter.java
@@ -1,38 +1,40 @@
 package frc.robot.Subsystems;
 
+import static frc.robot.Constants.Shooter.CURRENT_LIMIT;
+import static frc.robot.Constants.Shooter.ENCODER_ROTATIONS_TO_METERS;
+import static frc.robot.Constants.Shooter.LEFT_MOTOR_ID;
+import static frc.robot.Constants.Shooter.RIGHT_MOTOR_ID;
+import static frc.robot.Constants.Shooter.kA;
+import static frc.robot.Constants.Shooter.kD;
+import static frc.robot.Constants.Shooter.kI;
+import static frc.robot.Constants.Shooter.kP;
+import static frc.robot.Constants.Shooter.kS;
+import static frc.robot.Constants.Shooter.kV;
+
 import java.util.function.Supplier;
 
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.techhounds.houndutil.houndlib.SparkConfigurator;
 import com.techhounds.houndutil.houndlib.subsystems.BaseShooter;
 import com.techhounds.houndutil.houndlog.interfaces.Log;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-
-import com.revrobotics.CANSparkLowLevel;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-
-import edu.wpi.first.math.controller.ElevatorFeedforward;
-import edu.wpi.first.math.controller.PIDController;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
-
-import static frc.robot.Constants.Shooter.*;
 
 public class Shooter extends SubsystemBase implements BaseShooter {
 
     //example motor
-    private CANSparkFlex Left;
-    private CANSparkFlex Right;
+    private CANSparkFlex primaryMotor;
+    private CANSparkFlex secondaryMotor;
     //example velocity variable
 
     private SimpleMotorFeedforward feedforwardController = new SimpleMotorFeedforward(kS, kV, kA);
-    private PIDController PIDController = new PIDController(kP,kI,kD);
+    private PIDController pidController = new PIDController(kP,kI,kD);
 
     @Log(groups = "control")
     private double feedbackVoltage = 0;
@@ -41,13 +43,13 @@ public class Shooter extends SubsystemBase implements BaseShooter {
 
 
     public Shooter(){
-        Left = SparkConfigurator.createSparkFlex(
+        primaryMotor = SparkConfigurator.createSparkFlex(
                 LEFT_MOTOR_ID, MotorType.kBrushless, false,
                 (s) -> s.setIdleMode(IdleMode.kBrake),
                 (s) -> s.setSmartCurrentLimit(CURRENT_LIMIT),
                 (s) -> s.getEncoder().setPositionConversionFactor(ENCODER_ROTATIONS_TO_METERS),
                 (s) -> s.getEncoder().setVelocityConversionFactor(ENCODER_ROTATIONS_TO_METERS / 60.0));
-        Right = SparkConfigurator.createSparkFlex(
+        secondaryMotor = SparkConfigurator.createSparkFlex(
                 RIGHT_MOTOR_ID, MotorType.kBrushless, false,
                 (s) -> s.setIdleMode(IdleMode.kBrake),
                 (s) -> s.setSmartCurrentLimit(CURRENT_LIMIT),
@@ -57,32 +59,32 @@ public class Shooter extends SubsystemBase implements BaseShooter {
 
     }
 
-    public double getPosition(){
-        return (Left.getEncoder().getPosition() + Right.getEncoder().getPosition()) / 2;
+    public double getPosition(){ //not necessary
+        return (primaryMotor.getEncoder().getPosition() + secondaryMotor.getEncoder().getPosition()) / 2;
     }
 
     @Override
     public double getVelocity() {
-        return (Left.getEncoder().getVelocity() + Right.getEncoder().getVelocity()) / 2;
+        return (primaryMotor.getEncoder().getVelocity());
     }
 
     @Override
     public void setVoltage(double voltage) {
-        Left.setVoltage(voltage);
-        Right.setVoltage(voltage);
+        primaryMotor.setVoltage(voltage);
+        secondaryMotor.setVoltage(voltage);
     }
 
     @Override
     public Command spinAtVelocityCommand(Supplier<Double> goalVelocitySupplier) { //idk lol
         return run(() -> {
-            feedbackVoltage = PIDController.calculate(getVelocity());
+            feedbackVoltage = pidController.calculate(getVelocity(), goalVelocitySupplier.get());
             feedforwardVoltage = feedforwardController.calculate(goalVelocitySupplier.get());
             setVoltage(feedbackVoltage + feedforwardVoltage);
         });
     }
 
     @Override
-    public Command spinAtCurrentVelocityCommand() { //idk lol
+    public Command spinAtCurrentVelocityCommand() { //probably needs to be removed
         return runOnce(() -> spinAtVelocityCommand(() -> getVelocity()));
 
     }
@@ -95,15 +97,15 @@ public class Shooter extends SubsystemBase implements BaseShooter {
 
     @Override
     public Command coastMotorsCommand() { //idk
-        return runOnce(() -> Left.stopMotor())
+        return runOnce(() -> primaryMotor.stopMotor())
                 .andThen(() -> {
-                    Left.setIdleMode(IdleMode.kCoast);
-                    Right.setIdleMode(IdleMode.kCoast);
+                    primaryMotor.setIdleMode(IdleMode.kCoast);
+                    secondaryMotor.setIdleMode(IdleMode.kCoast);
                 })
                 .finallyDo((d) -> {
-                    Left.setIdleMode(IdleMode.kBrake);
-                    Right.setIdleMode(IdleMode.kBrake);
-                    PIDController.reset();
+                    primaryMotor.setIdleMode(IdleMode.kBrake);
+                    secondaryMotor.setIdleMode(IdleMode.kBrake);
+                    pidController.reset();
                 }).withInterruptBehavior(InterruptionBehavior.kCancelIncoming);
     }
 
