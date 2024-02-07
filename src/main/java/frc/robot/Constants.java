d6c31bea4a495b63eb6b2a4ff8bf39520e95c0a6
ZorzalG <nickwillimJT2@gmail.com>
2024-02-07 18:07:42 -0500

    add warning comments for gavin

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,8 +13,8 @@ import edu.wpi.first.math.util.Units;
 
 public class Constants {
     public static final class Vision {
-        public static final Matrix<N3, N1> SINGLE_TAG_STD_DEVS = VecBuilder.fill(4, 4, 8);
-        public static final Matrix<N3, N1> MULTI_TAG_STD_DEVS = VecBuilder.fill(0.5, 0.5, 1);
+        public static final Matrix<N3, N1> SINGLE_TAG_STD_DEVS = VecBuilder.fill(4, 4, 8); //TODO untested
+        public static final Matrix<N3, N1> MULTI_TAG_STD_DEVS = VecBuilder.fill(0.5, 0.5, 1); //TODO untested
 
         public static final PhotonCameraConstants CAMERA_CONSTANTS = new PhotonCameraConstants();
         static {
@@ -27,23 +27,24 @@ public class Constants {
         }
 
         // NOT FINAL! PLEASE CHANGE
-        // TODO
+        // TODO !!!!
 
         /** Calculations for transforming from camera location to robot location */
         public static final Transform3d[] ROBOT_TO_CAMS = new Transform3d[] {
                 new Transform3d(
-                        new Translation3d(Units.inchesToMeters(9.5), Units.inchesToMeters(9.5),
-                                Units.inchesToMeters(10)),
-                        new Rotation3d(0, -Units.degreesToRadians(10), -Math.PI / 8)),
+                        new Translation3d(Units.inchesToMeters(9.5), Units.inchesToMeters(9.5), //!!
+                                Units.inchesToMeters(10)), //!!
+                        new Rotation3d(0, -Units.degreesToRadians(10), -Math.PI / 8)), //!!
+                new Transform3d( 
+                        new Translation3d(Units.inchesToMeters(-12.5), Units.inchesToMeters(0), //!!
+                                Units.inchesToMeters(10)), //!!
+                        new Rotation3d(0, -Units.degreesToRadians(10), Math.PI)), //!!
                 new Transform3d(
-                        new Translation3d(Units.inchesToMeters(-12.5), Units.inchesToMeters(0),
-                                Units.inchesToMeters(10)),
-                        new Rotation3d(0, -Units.degreesToRadians(10), Math.PI)),
-                new Transform3d(
-                        new Translation3d(Units.inchesToMeters(9.5), Units.inchesToMeters(-9.5),
-                                Units.inchesToMeters(10)),
-                        new Rotation3d(0, -Units.degreesToRadians(10), Math.PI / 8))
+                        new Translation3d(Units.inchesToMeters(9.5), Units.inchesToMeters(-9.5), //!!
+                                Units.inchesToMeters(10)), //!!
+                        new Rotation3d(0, -Units.degreesToRadians(10), Math.PI / 8)) //!!
         };
+        //TODO all untested
 
     }
 
