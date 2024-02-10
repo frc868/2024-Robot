7470cb0c97ab1e63414b1b128a95c596b2a96f46
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-02-10 18:29:28 -0500

    climber cleanup + simulation support

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,37 +2,87 @@ package frc.robot.subsystems;
 
 import java.util.function.Supplier;
 
-import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.techhounds.houndutil.houndlib.SparkConfigurator;
 import com.techhounds.houndutil.houndlib.subsystems.BaseElevator;
-
+import com.techhounds.houndutil.houndlog.interfaces.Log;
+import com.techhounds.houndutil.houndlog.interfaces.LoggedObject;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
+import frc.robot.Constants.Climber.ClimberPosition;
 import static frc.robot.Constants.Climber.*;
 
+@LoggedObject
 public class Climber extends SubsystemBase implements BaseElevator<ClimberPosition> {
+    @Log
+    private CANSparkMax motor;
+
+    @Log
     private ProfiledPIDController pidController = new ProfiledPIDController(kP, kI, kD, MOVEMENT_CONSTRAINTS);
+
+    @Log
     private ElevatorFeedforward feedforwardController = new ElevatorFeedforward(kS, kG, kV, kA);
 
+    private ElevatorSim elevatorSim = new ElevatorSim(
+            MOTOR_GEARBOX_REPR,
+            GEARING,
+            CARRIAGE_MASS_KG,
+            DRUM_RADIUS_METERS,
+            MIN_HEIGHT_METERS,
+            MAX_HEIGHT_METERS,
+            true,
+            0);
+
+    @Log(groups = "control")
     private double feedbackVoltage = 0;
+    @Log(groups = "control")
     private double feedforwardVoltage = 0;
 
-    private CANSparkFlex motor = SparkConfigurator.createSparkFlex(MOTOR_ID, MotorType.kBrushless,
-            MOTOR_INVERTED, (motor) -> motor.setIdleMode(IdleMode.kBrake),
-            (motor) -> motor.setSmartCurrentLimit(CURRENT_LIMIT),
-            (motor) -> motor.getEncoder().setPositionConversionFactor(ENCODER_ROTATIONS_TO_METERS),
-            (motor) -> motor.getEncoder().setVelocityConversionFactor(ENCODER_ROTATIONS_TO_METERS / 60.0));
+    /**
+     * Initializes the elevator.
+     */
+    public Climber() {
+        motor = SparkConfigurator.createSparkMax(
+                MOTOR_ID, MotorType.kBrushless, false,
+                (s) -> s.setIdleMode(IdleMode.kBrake),
+                (s) -> s.setSmartCurrentLimit(CURRENT_LIMIT),
+                (s) -> s.getEncoder().setPositionConversionFactor(ENCODER_ROTATIONS_TO_METERS),
+                (s) -> s.getEncoder().setVelocityConversionFactor(ENCODER_ROTATIONS_TO_METERS / 60.0));
+
+        pidController.setTolerance(TOLERANCE);
+
+        setDefaultCommand(moveToCurrentGoalCommand());
+    }
+
+    /**
+     * Updates simulation-specific variables.
+     */
+    @Override
+    public void simulationPeriodic() {
+        elevatorSim.setInput(motor.getAppliedOutput());
+        elevatorSim.update(0.020);
+        motor.getEncoder().setPosition(elevatorSim.getPositionMeters());
+    }
 
     @Override
+    @Log
     public double getPosition() {
         return motor.getEncoder().getPosition();
+    }
+
+    @Log
+    public Pose3d getComponentPose() {
+        return BASE_COMPONENT_POSE.plus(new Transform3d(0, 0, getPosition(), new Rotation3d()));
     }
 
     @Override
@@ -52,15 +102,15 @@ public class Climber extends SubsystemBase implements BaseElevator<ClimberPositi
             feedforwardVoltage = feedforwardController.calculate(pidController.getSetpoint().position,
                     pidController.getSetpoint().velocity);
             setVoltage(feedbackVoltage + feedforwardVoltage);
-        }).withName("Move to Current Goal");
+        }).withName("climber.moveToCurrentGoal");
     }
 
     @Override
     public Command moveToPositionCommand(Supplier<ClimberPosition> goalPositionSupplier) {
         return Commands.sequence(
                 runOnce(() -> pidController.reset(getPosition())),
-                runOnce(() -> pidController.setGoal(goalPositionSupplier.get().position)),
-                moveToCurrentGoalCommand().until(pidController::atGoal)).withName("Move to Position");
+                runOnce(() -> pidController.setGoal(goalPositionSupplier.get().value)),
+                moveToCurrentGoalCommand().until(pidController::atGoal)).withName("climber.moveToPosition");
     }
 
     @Override
@@ -68,41 +118,40 @@ public class Climber extends SubsystemBase implements BaseElevator<ClimberPositi
         return Commands.sequence(
                 runOnce(() -> pidController.reset(getPosition())),
                 runOnce(() -> pidController.setGoal(goalPositionSupplier.get())),
-                moveToCurrentGoalCommand().until(pidController::atGoal)).withName("Move to Arbitrary Position");
+                moveToCurrentGoalCommand().until(pidController::atGoal)).withName("climber.moveToArbitraryPosition");
     }
 
     @Override
     public Command movePositionDeltaCommand(Supplier<Double> delta) {
         return moveToArbitraryPositionCommand(() -> pidController.getGoal().position + delta.get())
-                .withName("Move Position Delta");
+                .withName("climber.movePositionDelta");
     }
 
     @Override
     public Command holdCurrentPositionCommand() {
         return runOnce(() -> pidController.setGoal(getPosition())).andThen(moveToCurrentGoalCommand())
-                .withName("Hold Current Position");
+                .withName("climber.holdCurrentPosition");
     }
 
     @Override
     public Command resetPositionCommand() {
-        return runOnce(this::resetPosition).withName("Reset Position");
+        return runOnce(this::resetPosition).withName("climber.resetPosition");
     }
 
     @Override
     public Command setOverridenSpeedCommand(Supplier<Double> speed) {
-        return run(() -> setVoltage(12.0 * speed.get())).withName("Set Overridden Speed");
+        return run(() -> setVoltage(12.0 * speed.get()))
+                .withName("climber.setOverriddenSpeed");
     }
 
     @Override
     public Command coastMotorsCommand() {
-        return runOnce(() -> motor.stopMotor())
-                .andThen(() -> {
-                    motor.setIdleMode(IdleMode.kCoast);
-                })
+        return runOnce(motor::stopMotor)
+                .andThen(() -> motor.setIdleMode(IdleMode.kCoast))
                 .finallyDo((d) -> {
                     motor.setIdleMode(IdleMode.kBrake);
                     pidController.reset(getPosition());
-                }).withInterruptBehavior(InterruptionBehavior.kCancelIncoming).withName("Coast Motors");
+                }).withInterruptBehavior(InterruptionBehavior.kCancelIncoming)
+                .withName("climber.coastMotorsCommand");
     }
-
 }
