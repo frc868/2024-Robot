4ca4771bead8a80e5cec13e5cf0dc0e978a479bb
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-02-10 21:52:01 -0500

    set up houndrobot

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -4,13 +4,28 @@
 
 package frc.robot;
 
+import com.techhounds.houndutil.houndlib.robots.HoundRobot;
+
 import edu.wpi.first.wpilibj.RobotBase;
 
+/**
+ * Do NOT add any static variables to this class, or any initialization at all.
+ * Unless you know what
+ * you are doing, do not modify this file except to change the parameter class
+ * to the startRobot
+ * call.
+ */
 public final class Main {
     private Main() {
     }
 
+    /**
+     * Main initialization function. Do not perform any initialization here.
+     *
+     * <p>
+     * If you change your main robot class, change the parameter type.
+     */
     public static void main(String... args) {
-        RobotBase.startRobot(Robot::new);
+        RobotBase.startRobot(() -> new HoundRobot(() -> new RobotContainer()));
     }
 }
