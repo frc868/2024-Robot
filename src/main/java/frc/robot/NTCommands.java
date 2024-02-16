7419b53bf2a6fbf221d0d47cdbfe3f90c2dcfb0b
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-02-16 00:47:03 -0500

    Remove unused import

--- src/main/java/frc/robot/NTCommands.java
+++ src/main/java/frc/robot/NTCommands.java
@@ -7,7 +7,6 @@ import com.techhounds.houndutil.houndlog.loggers.SendableLogger;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.LEDs;
 import frc.robot.subsystems.NoteLift;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.ShooterTilt;
