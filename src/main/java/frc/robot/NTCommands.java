c8d83b2a6f8a3001af6fdd6c901280e916ad0b93
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-02-28 16:54:35 -0500

    Add LEDs to NTCommands

--- src/main/java/frc/robot/NTCommands.java
+++ src/main/java/frc/robot/NTCommands.java
@@ -7,14 +7,25 @@ import com.techhounds.houndutil.houndlog.loggers.SendableLogger;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.LEDs;
 import frc.robot.subsystems.NoteLift;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.ShooterTilt;
 
 public class NTCommands {
     public static void configureNTCommands(Drivetrain drivetrain, Intake intake, Shooter shooter,
-            ShooterTilt shooterTilt, Climber climber, NoteLift noteLift) {
+            ShooterTilt shooterTilt, Climber climber, NoteLift noteLift, LEDs leds) {
         LoggingManager.getInstance().addGroup(new LogGroup(
+                new SendableLogger("commands/leds", "chase",
+                        leds.requestChaseCommand()),
+                new SendableLogger("commands/leds", "wave",
+                        leds.requestWaveCommand()),
+                new SendableLogger("commands/leds", "red",
+                        leds.requestFlashingRedCommand()),
+                new SendableLogger("commands/leds", "breathe",
+                        leds.requestBreatheCommand()),
+                new SendableLogger("commands/leds", "fire",
+                        leds.requestFireCommand()),
                 new SendableLogger("commands/intake", "resetPosition",
                         intake.resetPositionCommand().ignoringDisable(true)),
                 new SendableLogger("commands/shooterTilt", "resetPosition",
