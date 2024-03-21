bc6245414b6d10c026d239db41400e70387cdcb0
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2024-03-21 13:08:56 -0400

    Ignore disable on houndbrian

--- src/main/java/frc/robot/HoundBrian.java
+++ src/main/java/frc/robot/HoundBrian.java
@@ -51,22 +51,22 @@ public class HoundBrian {
                 // .and(new Trigger(intakeButton::get).negate().debounce(2,
                 // DebounceType.kFalling))
                 .and(DriverStation::isDisabled)
-                .onTrue(intake.resetPositionCommand());
+                .onTrue(intake.resetPositionCommand().ignoringDisable(true));
         new Trigger(shooterTiltButton::get).negate()
                 // .and(new Trigger(shooterTiltButton::get).negate().debounce(2,
                 // DebounceType.kFalling))
                 .and(DriverStation::isDisabled)
-                .onTrue(shooterTilt.resetPositionCommand());
+                .onTrue(shooterTilt.resetPositionCommand().ignoringDisable(true));
         new Trigger(climberButton::get).negate()
                 // .and(new Trigger(climberButton::get).negate().debounce(2,
                 // DebounceType.kFalling))
                 .and(DriverStation::isDisabled)
-                .onTrue(climber.resetPositionCommand());
+                .onTrue(climber.resetPositionCommand().ignoringDisable(true));
         new Trigger(noteLiftButton::get).negate()
                 // .and(new Trigger(noteLiftButton::get).negate().debounce(2,
                 // DebounceType.kFalling))
                 .and(DriverStation::isDisabled)
-                .onTrue(noteLift.resetPositionCommand());
+                .onTrue(noteLift.resetPositionCommand().ignoringDisable(true));
 
         // new Trigger(intakeButton::get).debounce(3, DebounceType.kBoth)
         // .and(DriverStation::isDisabled)
