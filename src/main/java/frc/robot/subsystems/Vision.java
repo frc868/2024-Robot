497a53c2914a4110167defe4a309c2d6c5780b2d
QuinnKenison <quinnkenison@gmail.com>
2025-10-24 18:52:40 -0400

    Update houndutil

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -107,13 +107,13 @@ public class Vision extends SubsystemBase {
 
     @Log(groups = "cameras")
     private final AprilTagPhotonCamera houndeye01 = new AprilTagPhotonCamera("HoundEye01",
-            ROBOT_TO_CAMS[0], CAMERA_CONSTANTS, 0.2, 0.1);
+            ROBOT_TO_CAMS[0], CAMERA_CONSTANTS, 0.2, 0.1, AprilTagFields.k2024Crescendo);
     @Log(groups = "cameras")
     private final AprilTagPhotonCamera houndeye02 = new AprilTagPhotonCamera("HoundEye02",
-            ROBOT_TO_CAMS[1], CAMERA_CONSTANTS, 0.2, 0.1);
+            ROBOT_TO_CAMS[1], CAMERA_CONSTANTS, 0.2, 0.1, AprilTagFields.k2024Crescendo);
     @Log(groups = "cameras")
     private final AprilTagPhotonCamera houndeye03 = new AprilTagPhotonCamera("HoundEye03",
-            ROBOT_TO_CAMS[2], CAMERA_CONSTANTS, 0.2, 0.1);
+            ROBOT_TO_CAMS[2], CAMERA_CONSTANTS, 0.2, 0.1, AprilTagFields.k2024Crescendo);
 
     private final AprilTagPhotonCamera[] cameras = new AprilTagPhotonCamera[] {
             houndeye01, houndeye02, houndeye03 };
